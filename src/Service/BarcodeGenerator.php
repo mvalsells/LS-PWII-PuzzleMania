eae20b156574099e93d6d0a025afd3af38aeb09b
Marc Valsells Niubó <marc@valsells.me>
2023-04-25 19:18:33 +0200

    feat(Barcode): Added comments

--- src/Service/BarcodeGenerator.php
+++ src/Service/BarcodeGenerator.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * Barcode class: Provides access to the BarcodePro Web API for Docker by Neodynamic
+ * @author: Marc Valsells, Òscar de Jesus and David Larrosa
+ * @creation: 24/05/2023
+ * @updated: 25/05/2023
+ */
 declare(strict_types=1);
 
 namespace Salle\PuzzleMania\Service;
@@ -9,23 +14,40 @@ use GuzzleHttp;
 class BarcodeGenerator
 {
     private GuzzleHttp\Client $client;
+
+    /**
+     * Constructor for a BarcodeGenerator object
+     */
     public function __construct(){
         $this->client = new GuzzleHttp\Client([
             'base_uri' => "http://barcode"
         ]);
     }
 
-    public function simpleQRBase64(string $data): string{
-        $response = $this->client->post('/BarcodeGenerator', [
-            GuzzleHttp\RequestOptions::HEADERS => [
-                'Accept' => 'image/jpeg'
-            ],
-            GuzzleHttp\RequestOptions::JSON => [
-                'symbology' => 'QRCode',
-                'code' => $data,
-                'artFinderShape' => 'RoundRect',
-            ]
-        ]);
+    /**
+     * Generates QR code with the API
+     * @param string $data Data to be found in the QR code
+     * @return string|null Image with the mime and data encoded in base64, ready to be placed in the src attribute of
+     *                     an img tag. If there was an error generating the code null is returned.
+     */
+    public function simpleQRBase64(string $data): ?string{
+        try {
+            $response = $this->client->post('/BarcodeGenerator', [
+                // Answer must be a jpeg file
+                GuzzleHttp\RequestOptions::HEADERS => [
+                    'Accept' => 'image/jpeg'
+                ],
+                // Barcode format and data
+                GuzzleHttp\RequestOptions::JSON => [
+                    'symbology' => 'QRCode',
+                    'code' => $data,
+                    'artFinderShape' => 'RoundRect',
+                ]
+            ]);
+        } catch (GuzzleHttp\Exception\GuzzleException $e) {
+            return null;
+        }
+        // Get the image data from the request and encode it in base64
         $imgData = base64_encode($response->getBody()->getContents());
         return 'data:image/jpeg;base64,' . $imgData;
     }

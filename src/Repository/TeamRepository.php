560cde8407a51b1b3267f91ddadbe739229c5c20
Òscar de Jesús Ruiz <oscar.dejesus@students.salle.url.edu>
2023-04-28 11:14:16 +0200

    Team Repository Implementation

---
+++ src/Repository/TeamRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Salle\PuzzleMania\Repository;
+
+use Salle\PuzzleMania\Model\Team;
+use Salle\PuzzleMania\Model\User;
+
+interface TeamRepository
+{
+    public function getTeamByUserId(int $user_id): Team;
+
+    public function getTeamById(int $id): Team;
+
+    public function createTeam(Team $team): void;
+
+    public function addUserToTeam(int $team_id, User $user): void;
+
+    public function getIncompleteTeams(): array;
+
+    public function addScoreToTeam(int $team_id, int $score): void;
+}
